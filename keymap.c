7f66a66dd1e67300c7adba0cd4ca56115f4c0f5f
kdlr <sailorfe@proton.me>
2023-11-14 10:56:33 -0500

    moved functions

--- keymap.c
+++ keymap.c
@@ -2,10 +2,10 @@
 
 enum planck_layers {
     _QWERTY,
-    _SYMBOLS,
-    _NUMBERS,
+    _LOWER,
+    _RAISE,
     _VIM,
-    _FNPAD,
+    _NUMPAD,
     _ADJUST,
     _CELLO,
     _GUITAR
@@ -15,10 +15,10 @@ enum planck_keycodes {
     QWERTY = SAFE_RANGE
 };
 
-#define SYM     MO(_SYMBOLS)
-#define NUM     MO(_NUMBERS)
+#define LOWER   MO(_LOWER
+#define RAISE   MO(_RAISE)
 #define VIM     MO(_VIM)
-#define FN      MO(_FNPAD)
+#define PAD     MO(_NUMPAD)
 #define ADJUST  MO(_ADJUST)
 #define CELLO   TG(_CELLO)
 #define GUITAR  TG(_GUITAR)
@@ -32,29 +32,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
         KC_ESC, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
         KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_ENT,
-        FN,     KC_LCTL,KC_LGUI,KC_LALT,SYM,    VIM,    KC_SPC, NUM,    ALT_SFT,GUI_SFT,CTL_SFT,CW_TOGG
+        PAD,    KC_LCTL,KC_LGUI,KC_LALT,SYM,    VIM,    KC_SPC, NUM,    ALT_SFT,GUI_SFT,CTL_SFT,CW_TOGG
     ),
-    [_SYMBOLS] = LAYOUT_planck_grid(
+    [_LOWER] = LAYOUT_planck_grid(
         KC_TILD,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,_______,
-        KC_DEL, _______,_______,_______,_______,_______,_______,KC_UNDS,KC_PLUS,KC_LCBR,KC_RCBR,KC_PIPE,
-        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,_______,ADJUST, KC_MUTE,KC_VOLD,KC_VOLU,KC_MPLY
+        KC_DEL, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_UNDS,KC_PLUS,KC_LCBR,KC_RCBR,KC_PIPE,
+        _______,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,_______,_______,_______,_______,
+        _______,_______,_______,_______,_______,_______,_______,iADJUST, KC_MUTE,KC_VOLD,KC_VOLU,KC_MPLY
     ),
-    [_NUMBERS] = LAYOUT_planck_grid(
+    [_RAISE] = LAYOUT_planck_grid(
         KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   _______,
-        KC_DEL, _______,_______,_______,_______,_______,_______,KC_MINS,KC_EQL, KC_LBRC,KC_RBRC,KC_BSLS,
-        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+        KC_DEL, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_MINS,KC_EQL, KC_LBRC,KC_RBRC,KC_BSLS,
+        _______,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,_______,_______,_______,_______,
         _______,_______,_______,_______,ADJUST, _______,_______,_______,_______,_______,_______,_______
     ),
     [_VIM] = LAYOUT_planck_grid(
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
-        KC_ESC, _______,_______,_______,KC_PGDN,KC_HOME,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_COLN,_______,
+        KC_ESC, _______,_______,_______,KC_PGDN,KC_HOME,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,_______,_______,
         _______,_______,_______,_______,_______,KC_PGUP,KC_END, _______,_______,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______
     ),
-    [_FNPAD] = LAYOUT_planck_grid(
-        _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  _______,KC_7,   KC_8,   KC_9,   KC_SLSH,
-        _______,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,KC_4,   KC_5,   KC_6,   KC_ASTR,
+    [_NUMPAD] = LAYOUT_planck_grid(
+        _______,_______,KC_W,   _______,_______,_______,_______,_______,KC_7,   KC_8,   KC_9,   KC_SLSH,
+        _______,KC_A,   KC_S,   KC_D,   _,_______,_______,_______,_______,KC_4,   KC_5  KC_6,   KC_ASTR,
         _______,_______,_______,_______,_______,_______,_______,_______,KC_1,   KC_2,   KC_3,   KC_MINS,
         _______,_______,_______,_______,_______,_______,_______,_______,KC_DOT, KC_0,   KC_ENT, KC_PLUS 
     ),
